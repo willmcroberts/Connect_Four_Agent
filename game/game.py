7fd998d8705ac880e4f599ef71deddeb561c172d
willmcroberts <williammcroberts15@gmail.com>
2026-04-07 11:24:31 -0400

    Create needed functions

--- game/game.py
+++ game/game.py
@@ -4,7 +4,8 @@ from . import minimax
 
 class connect_four:
 
-    self.board = [[0 for _ in range(7)] for _ in range(6)]
+    def __init__(self):
+        self.board = [[0 for _ in range(7)] for _ in range(6)]
 
     def legal_moves(self):
         return [c for c in range(7) if self.board[0][c] == 0]
@@ -15,3 +16,15 @@ class connect_four:
                 self.board[row][column] = player
                 return row, column
         raise ValueError("Column is full")
+
+    def check_win(self, player):
+        print()
+
+    def is_draw(self):
+        print()
+
+    def copy(self):
+        print()
+
+    def print_board(self):
+        print()
