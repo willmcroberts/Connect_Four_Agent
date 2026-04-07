c1969cca005a7371077e9f8decfe957bdb8d28aa
willmcroberts <williammcroberts15@gmail.com>
2026-04-07 11:20:53 -0400

    Create apply_move

--- game/game.py
+++ game/game.py
@@ -9,3 +9,9 @@ class connect_four:
     def legal_moves(self):
         return [c for c in range(7) if self.board[0][c] == 0]
 
+    def apply_move(self, column, player):
+        for row in range(5, -1, -1):
+            if self.board[row][column] ==0:
+                self.board[row][column] = player
+                return row, column
+        raise ValueError("Column is full")
